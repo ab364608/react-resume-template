5b3fcf5b7ab262d66409d5bd28e695118e55789d
Ben Anderson <ben@Bens-MBP.home>
2020-01-16 15:49:28 -0600

    changed profile image

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use( express.static( `${__dirname}/../build` ) );
+
 const PORT = 5500;
 app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`)
